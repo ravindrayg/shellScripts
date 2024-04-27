00c16cadf36cc9aa3747f154d42c4cb1ced8d77f
ravindra <ravindrayerramsetti9@gmail.com>
2024-04-27 15:11:31 +0530

    m4
    m5

---
+++ script0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Description:
+#Author:
+#Created:
+#Mofified:
+#Version
+echo $0
+echo "===="
+echo $1  #first argument
+echo $2  #second argument
+echo $3
+echo "===="
+echo $#  #the count of number of arguments
+echo $*  #a list of all the arguments we passed
+echo $$  # running process id
+echo $?  # return value - earlier command successful/unsuccessful
